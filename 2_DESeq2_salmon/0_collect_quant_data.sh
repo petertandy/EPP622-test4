39510ddd97b16bbf68bbddc4cba0606aabefd237
Peter Tandy <tandy.noah@gmail.com>
2022-12-07 13:48:16 -0500

    Make gff more palatable for git

--- 2_DESeq2_salmon/0_collect_quant_data.sh
+++ 2_DESeq2_salmon/0_collect_quant_data.sh
@@ -5,3 +5,12 @@ for folder in ../0_raw_data/salmon_quants/*; do
 	sample_name=${sample_name/_quant}
 	cp ${folder}/quant.sf ${sample_name}.sf
 done
+echo "Copied and renamed the quantity files from 0_raw_data/salmon_quants/(sample name)/quant.sf to 2_DESeq_salmon/(sample name).sf"
+
+# make sure the gff3 file is unzipped for use in analysis
+if [ -f ../0_raw_data/Ppersica_298_v2.1.gene_exons.gff3 ]; then
+	echo "Not unzipping the gff3 file (../0_raw_data/Ppersica_298_v2.1.gene_exons.gff3.gz), as it appears to already be unzipped."
+else
+	gzip -dk "../0_raw_data/Ppersica_298_v2.1.gene_exons.gff3.gz"
+	echo "Unzipped the gff3 file (../0_raw_data/Ppersica_298_v2.1.gene_exons.gff3.gz)."
+fi
